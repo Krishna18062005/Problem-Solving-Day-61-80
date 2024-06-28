870f3637203ef6739b6c6516221c91923e16d0de
Krishna K <krish1826s@gmail.com>
2024-06-28 08:51:28 +0530

    Update day_67.java

--- day_67.java
+++ day_67.java
@@ -18,3 +18,28 @@ public class Hello {
 
 	}
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Matrix -Row&Column Pattern
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		int c=sc.nextInt();
+		int[][] ar=new int[r][c];
+		for(int i=0;i<r;i++) for(int j=0;j<c;j++) ar[i][j]=sc.nextInt();
+		for(int i=0;i<Math.min(r,c);i++){
+		    for(int j=i;j<r;j++){
+		        System.out.print(ar[i][j]+" ");
+		    }
+		    System.out.println();
+		    for(int k=i+1;k<Math.min(r,c);k++){
+		        System.out.print(ar[k][i]+" ");
+		    }
+		    if(i!=r-1)
+		    System.out.println();
+		}
+		
+
+	}
+}
