dbfb9d37bec30c92d7ff05262e3f6f6562e36176
Krishna K <krish1826s@gmail.com>
2024-06-30 09:10:36 +0530

    Update day_69.java

--- day_69.java
+++ day_69.java
@@ -1,1 +1,41 @@
 
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Matrix -String Border;
+		Scanner sc=new Scanner(System.in);
+		String[] ip=sc.nextLine().split(" ");
+		int len=ip[0].length();
+		char[][] ma=new char[len][len];
+		for(int i=0;i<len;i++) for(int j=0;j<len;j++) ma[i][j]=sc.next().charAt(0);
+		StringBuilder[] str=new StringBuilder[4];
+		for(int i=0;i<4;i++) str[i]=new StringBuilder("");
+		for(int i=0;i<len;i++) {
+		   
+		        str[0].append(ma[0][i]);
+		    
+		        str[1].append(ma[i][0]);
+		    
+		        str[2].append(ma[len-1][i]);
+		        
+		        str[3].append(ma[i][len-1]);
+		}
+		System.out.print(check(ip,str));
+
+	}
+	static String check(String[] a,StringBuilder[] b){
+	    for(int i=0;i<a.length;i++){
+	        int j=-1;
+	        for(j=0;j<4;j++){
+	            if(b[j].toString().equals(a[i])||b[j].reverse().toString().equals(a[i])){
+	                b[j]=new StringBuilder("");
+	                break;
+	            }
+	            
+	        }
+	        if(j==4) return "No";
+	    }
+	    return "Yes";
+	}
+}
