18511782656a146ab39434909fa4256bf75ab9d9
Krishna K <krish1826s@gmail.com>
2024-07-02 09:46:44 +0530

    Update day_71.java

--- day_71.java
+++ day_71.java
@@ -18,3 +18,26 @@ public class Hello {
 	    return st.charAt(0)=='1';
 	}
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//String -Common Words
+		Scanner sc=new Scanner(System.in);
+		String[] fs=sc.nextLine().split(" ");
+		String[] ls=sc.nextLine().split(" ");
+		int k=sc.nextInt();
+	
+	for(String i:fs){
+	    if(i.length()!=k) continue;
+	    for(String j:ls){
+	        if(j.length()!=k) continue;
+	        if(i.equals(j)){
+	            System.out.print(j+" ");
+	            break;
+	        }
+	    }
+	}
+
+	}
+}
