bbe66506ea5c1fcda4c0016674cb1f64267c280f
Krishna K <krish1826s@gmail.com>
2024-07-03 09:47:29 +0530

    Update day_72.java

--- day_72.java
+++ day_72.java
@@ -31,3 +31,32 @@ public class Hello {
 
 	}
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Shift Digits & Double
+		String s=new Scanner(System.in).next();
+		
+		StringBuilder ev=new StringBuilder();
+		int le=s.length();
+		
+		
+		
+		for(int i=1;i<le;i+=2){
+		    ev.append(s.charAt(i));
+		}
+		String even=ev.charAt(ev.length()-1)+ev.substring(0,ev.length()-1);
+		
+		long ans=0;
+		int pi=0;
+		for(int i=0;i<le;i+=2){
+		    ans=ans*10+(s.charAt(i)-'0');
+		    if(pi<even.length())
+		    ans=ans*10+(even.charAt(pi)-'0');
+		    pi++;
+		}
+		System.out.print(ans*2);
+
+	}
+}
