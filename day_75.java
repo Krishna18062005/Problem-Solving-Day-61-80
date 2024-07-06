b22e45f29e1098362f857b0455efaf6bd7b3e556
Krishna K <krish1826s@gmail.com>
2024-07-06 12:31:19 +0530

    Update day_75.java

--- day_75.java
+++ day_75.java
@@ -25,3 +25,41 @@ class Solution {
         return max;
     }
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Average of Largest Integers;
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		int c=sc.nextInt();
+		int[][] matr=new int[r][c];
+		int[] rowMax=new int[r];
+		int[] columnMax=new int[c];
+		for(int i=0;i<r;i++){
+		    int max=0;
+		    for(int j=0;j<c;j++){
+		        matr[i][j]=sc.nextInt();
+		        max=Math.max(max,matr[i][j]);
+		    }
+		    rowMax[i]=max;
+		}
+		for(int i=0;i<c;i++){
+		    int max=0;
+		    for(int j=0;j<r;j++){
+		        max=Math.max(max,matr[j][i]);
+		    }
+		    columnMax[i]=max;
+		}
+		
+		int s=0;
+		for(int i:rowMax) s+=i;
+		double rowAvg=(double)s/r;
+		System.out.printf("%.2f\n",rowAvg);
+		s=0;
+		for(int i:columnMax) s+=i;
+		double colAvg=(double)s/c;
+		System.out.printf("%.2f",colAvg);
+
+	}
+}
